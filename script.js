a81374ac2ff30a663f26550370a346edf0a30747
Maxime <max.boistelle@hotmail.fr>
2024-04-16 19:57:17 +0200

    update image of traine when data.result = false

--- script.js
+++ script.js
@@ -8,35 +8,39 @@ document.querySelector("#searchBtn").addEventListener("click", function () {
   )
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      if (data.result === false) {
+        document.querySelector("#train").src = "./assets/images/notfound.png";
+        document.querySelector("#text").textContent = "No trip found";
+      } else {
+        document.querySelector("imageTrain").style.display = "none";
+        for (const value of data.message) {
+          let newHour = new Date(value.date).getHours();
+          let newMinutes = new Date(value.date).getMinutes();
+          if (newMinutes < 10) {
+            newMinutes = "0" + newMinutes;
+          }
 
-      for (const value of data.message) {
-        let newHour = new Date(value.date).getHours();
-        let newMinutes = new Date(value.date).getMinutes();
-        if (newMinutes < 10) {
-          newMinutes = "0" + newMinutes;
-        }
-
-        document.querySelector(
-          ".block2"
-        ).innerHTML += `<div class ="total"><div class="price"><div class="revealSearch">
+          document.querySelector(
+            ".block2"
+          ).innerHTML += `<div class ="total"><div class="price"><div class="revealSearch">
             ${value.departure} > ${value.arrival}&nbsp;&nbsp;&nbsp;${newHour}:${newMinutes}&nbsp;&nbsp;&nbsp;<strong>${value.price}€</strong>
           </div></div><button id="bookCart">Book</button></div>`;
-        for (let i = 0; i < document.querySelectorAll(".total").length; i++) {
-          let newTravel = {
-            id: value._id,
-          };
-          document
-            .querySelectorAll(".total")
-            [i].addEventListener("click", function () {
-              fetch("http://localhost:3000/cart", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(newTravel),
-              })
-                .then((response) => response.json())
-                .then((data) => console.log(data));
-            });
+          for (let i = 0; i < document.querySelectorAll(".total").length; i++) {
+            let newTravel = {
+              id: value._id,
+            };
+            document
+              .querySelectorAll(".total")
+              [i].addEventListener("click", function () {
+                fetch("http://localhost:3000/cart", {
+                  method: "POST",
+                  headers: { "Content-Type": "application/json" },
+                  body: JSON.stringify(newTravel),
+                })
+                  .then((response) => response.json())
+                  .then((data) => console.log(data));
+              });
+          }
         }
       }
     });
